fc18d609d41011bb4119df931daf6a203aec00ac
Edward Popescu <edwardp383@Edwards-MacBook-Pro.local>
2019-03-27 12:15:33 -0500

    Base for ships

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Space Battle!!!!!")
+class Ship {
+	constructor(hull, firepower, accuracy) {
+		this.hull = hull;
+		this.firepower = firepower;
+		this.accuracy = accuracy
+	}
+
+}
+
+const hero = new Ship(20, 5, .7)
+const alienFactory = {
+	aliens: [],
+	generateShip(){
+		const newAlien = new Ship(
+				Math.floor(Math.random() * ((6 - 3) +1) + 3),
+				Math.floor(Math.random() * ((4 - 2) + 1) + 2),
+				Math.random() * (.8 - .6) + .6
+			);
+		this.aliens.push(newAlien);
+		return newAlien;
+	}
+
+}
+console.log(alienFactory.generateShip())
+console.log(aliens);
+
